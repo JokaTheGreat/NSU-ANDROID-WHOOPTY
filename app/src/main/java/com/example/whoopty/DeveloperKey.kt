a7e139850a03b3936b5a6d479417a636c927dc46
Nikita Latysh <n.latysh@g.nsu.ru>
2022-11-27 19:04:57 +0700

    Added meal card layouts

---
+++ app/src/main/java/com/example/whoopty/DeveloperKey.kt
@@ -0,0 +1,5 @@
+package com.example.whoopty
+
+class DeveloperKey {
+    val DEVELOPER_KEY: String = "AIzaSyARRemagFdN99t_8HWF-m7gdLgmSFwoczM"
+}
--- app/src/main/java/com/example/whoopty/MainActivity.kt
+++ app/src/main/java/com/example/whoopty/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.whoopty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.youtube.player.*
+
+/*
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import me.relex.circleindicator.CircleIndicator3
 
@@ -33,5 +38,31 @@ class MainActivity : AppCompatActivity() {
         for (i in 1..14) {
             addToList("BEEF", "Beef is the culinary name for meat from cattle, particularly skeletal muscle. Humans have been eating beef since prehistoric times.[1] Beef is a source of high-quality protein and essential nutrients.[2]", R.drawable.beef)
         }
+    }
+}
+*/
+
+class MyYoutubeListener : YouTubeThumbnailView.OnInitializedListener {
+    override fun onInitializationSuccess(p0: YouTubeThumbnailView?, p1: YouTubeThumbnailLoader?) {
+        p1?.setVideo("nMyBC9staMU")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubeThumbnailView?,
+        p1: YouTubeInitializationResult?
+    ) {
+        println(p0)
+        println(p1)
+        println("failed initialization youtubeeee")
+    }
+
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.meal_card_links_item)
+
+        findViewById<YouTubeThumbnailView>(R.id.youtube_thumbnail).initialize(DeveloperKey().DEVELOPER_KEY, MyYoutubeListener())
     }
 }
